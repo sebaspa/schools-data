97b8b1259dbc72564e6a5b67dd7389836c55504d
Sebaspa <sebaspa0107@gmail.com>
2021-08-31 21:54:36 -0500

    Se instala y usa el paquete para data tables

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Yajra\DataTables\DataTables;
 
 class UserController extends Controller
 {
@@ -90,59 +91,7 @@ class UserController extends Controller
      */
     public function get(Request $request)
     {
-        //Leer valores
-        $draw = $request->get('draw');
-        $start = $request->get('start');
-        $rowperpage = $request->get('length');
-
-        $columnIndex_arr = $request->get('order');
-        $columnName_arr = $request->get('columns');
-        $order_arr = $request->get('order');
-        $search_arr = $request->get('search');
-
-        $columnIndex = $columnIndex_arr[0]['column'];
-        $columnName = $columnName_arr[$columnIndex]['data'];
-        $columnSortOrder = $order_arr[0]['dir']; //asc or desc
-        $searchValue = $search_arr['value'];
-
-        //Todos los registros
-        $totalRecords = User::select('count(*) as allcount')->count();
-
-        //Total de registros por busqueda
-        $totalRecordsWithFilter = User::select('count(*) as allcount')
-            ->where('name', 'like', '%' . $searchValue . '%')
-            ->count();
-
-        $records = User::orderby($columnName, $columnSortOrder)
-            ->where('users.name', 'like', '%' . $searchValue . '%')
-            ->select('users.*')
-            ->skip($start)
-            ->take($rowperpage)
-            ->get();
-
-        $data_arr = array();
-        foreach ($records as $record) {
-            $name = $record->name;
-            $last_name = $record->last_name;
-            $email = $record->email;
-            $created_at = $record->created_at;
-
-            $data_arr[] = array(
-                "name" => $name,
-                "last_name" => $last_name,
-                "email" => $email,
-                "created_at" => $created_at,
-            );
-        }
-
-        $response = array(
-            "draw" => intval($draw),
-            "iTotalRecords" => $totalRecords,
-            "iTotalDisplayRecords" => $totalRecordsWithFilter,
-            "aaData" => $data_arr
-        );
-
-        echo json_encode($response);
-        exit;
+        $users = User::select('name', 'last_name', 'email', 'created_at')->get();
+        return DataTables::of($users)->make(true);
     }
 }
