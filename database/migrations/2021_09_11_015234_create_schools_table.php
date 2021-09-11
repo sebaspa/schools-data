036d4e208c6507f2f195bb4a7411d5094074bdbd
Sebaspa <sebaspa0107@gmail.com>
2021-09-10 19:24:36 -0500

    Creacion de migracion de escuelas

---
+++ database/migrations/2021_09_11_015234_create_schools_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSchoolsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('schools', function (Blueprint $table) {
+            $table->id();
+            $table->string("name", 200);
+            $table->string("address", 200);
+            $table->string("district", 200);
+            $table->string("phone", 50);
+            $table->string("email", 100);
+            $table->string("liable", 100);
+            $table->text("others");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('schools');
+    }
+}
