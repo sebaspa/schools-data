90d1ef4856670e1830eb85eddb7bd95ce5cfb8ec
Sebaspa <sebaspa0107@gmail.com>
2021-10-07 21:21:45 -0500

    Se acmbia las card por amarillo

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,7 +8,7 @@
 @section('content')
     <div class="row">
         <div class="col-12 col-md-4 col-lg-3">
-            <div class="small-box bg-info">
+            <div class="small-box bg-warning">
                 <div class="inner">
                     <h3>{{ $users }}</h3>
                     <p>Usuarios</p>
@@ -22,7 +22,7 @@
             </div>
         </div>
         <div class="col-12 col-md-4 col-lg-3">
-            <div class="small-box bg-info">
+            <div class="small-box bg-warning">
                 <div class="inner">
                     <h3>{{ $schools }}</h3>
                     <p>Escuelas</p>
