87e486e4df2607b216f9f3ec91491c77fb99ad20
Sebaspa <sebaspa0107@gmail.com>
2021-10-29 18:47:10 -0500

    Auditoria a servicios

--- app/Models/Service.php
+++ app/Models/Service.php
@@ -2,12 +2,14 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Spatie\Activitylog\LogOptions;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Activitylog\Traits\LogsActivity;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Service extends Model
 {
-    use HasFactory;
+    use HasFactory, LogsActivity;
 
     /**
      * The attributes that are mass assignable.
@@ -17,4 +19,16 @@ class Service extends Model
     protected $fillable = [
         'name',
     ];
+
+    /**
+     * Activity log
+     */
+
+    public function getActivitylogOptions(): LogOptions
+    {
+        return LogOptions::defaults()
+            ->useLogName('school_service')
+            ->setDescriptionForEvent(fn (string $eventName) => "Se ha " . trans('logs.' . $eventName) . " el servicio  :subject.id")
+            ->logOnlyDirty();
+    }
 }
